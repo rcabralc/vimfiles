3403212a431604612e27edd772c03d0a68e38ee9
Rafael Cabral Coutinho <ed@tecnologiaenegocios.com.br>
2011-06-03 12:29:29 -0300

    Updated pyflakes, now using it with pathogen

--- install.sh
+++ install.sh
@@ -9,4 +9,10 @@ git clone http://github.com/tpope/vim-rails.git/
 git clone http://github.com/astashov/vim-ruby-debugger.git/
 git clone http://github.com/tpope/vim-surround.git/
 git clone http://github.com/mattn/zencoding-vim.git/
+git clone http://github.com/kevinw/pyflakes-vim.git
+# clone the PyFlakes fork.
+rm -rf pyflakes-vim/ftplugin/python/pyflakes/
+cd pyflakes-vim/ftplugin/python/
+git clone http://github.com/kevinw/pyflakes.git
+cd -
 cd ../
