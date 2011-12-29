aee511bd419112c1cfdf7cfa75d1624b6ebc2cb1
rcabralc <rcabralc@gmail.com>
2011-12-29 11:56:39 -0200

    Review of submodules

--- install.sh
+++ install.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
 
 # Update submodules
-git submodule update --init
-
-# Syntax checking for python.
-# Clone the PyFlakes fork.  We can't do this using the submodule because its
-# url requires authentication.
-rm -rf bundle/pyflakes-vim/ftplugin/python/pyflakes/
-cd bundle/pyflakes-vim/ftplugin/python/
-git clone git://github.com/kevinw/pyflakes.git
-cd -
+git submodule update --init --recursive
